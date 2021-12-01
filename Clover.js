94192cf45151d81e716569b5bec7e64ef4e1be19
kauntak <n.koshirae+github@gmail.com>
2021-12-01 00:36:19 -0700

    Update Clover.js
    Refactored payment search loop.
    Updated payment test data.
    Updated comments to be clearer.
    Added payment failed functionality.
    Will leave failed payments in paymentData array after loop is finished.
    If employee is not found, it will re-load employees.

--- Clover.js
+++ Clover.js
@@ -22,48 +22,151 @@ let employeeList = [];
 let paymentData = [];
 let readDate = new Date();
 let isTest = false;
-let testData = [{
-    id: '0AT48JQ2B7FXCX6',
-    employee: { id: 'EXQJ6ED55DAB7JPDR', name: 'Non' },
-    time: '2021-11-21 12:25:47',
-    amount: 100.00,
-    tipAmount: 10,
-    result: 'SUCCESS'
-  },
-  {
-    id: 'Z8DNBEWGN44BF02',
-    employee: { id: 'EXQJ6ED55DAB7JPDR', name: 'Non' },
-    time: '2021-11-21 12:24:24',
-    amount: 82.18,
-    tipAmount: 6.74,
-    result: 'SUCCESS'
-  },
-  {
-    id: 'RXSC3A2QBPV8YB8',
-    employee: { id: 'EXQJ6ED55DAB7JPDR', name: 'Non' },
-    time: '2021-11-21 12:12:31',
-    amount: 17.59,
-    tipAmount: 19.93,
-    result: 'SUCCESS'
-  },
-  {
-    id: 'RXSC3A2QBPV8YB82',
-    employee: { id: 'EXQJ6ED55DAB7JPDR', name: 'Non' },
-    time: '2021-11-21 12:12:31',
-    amount: 17.59,
-    tipAmount: 19.93,
-    result: 'FAILED'
-  },
-  {
-    id: '4HB4ER6BTVRAPN0',
-    employee: { id: 'EXQJ6ED55DAB7JPDR', name: 'Non' },
-    time: '2021-11-21 12:10:54',
-    amount: 215.67,
-    tipAmount: 0,
-    result: 'SUCCESS'
-  }
-];
-
+let testData = [
+	{
+	  "id": "1CHS0KZ4AG85V64",
+	  "employee": {
+		"id": "4SCX9K33S7SKR",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 16:34:22",
+	  "amount": 72.19,
+	  "tipAmount": 3,
+	  "result": "SUCCESS",
+	},
+	{
+	  "id": "YV0DAF7X9D1VF9P",
+	  "employee": {
+		"id": "4SCX9K33S7SKR",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 16:42:00",
+	  "amount": 16.49,
+	  "tipAmount": 2.47,
+	  "result": "SUCCESS",
+	},
+	{
+	  "id": "VXHA75FMQVG8VZE",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 16:41:45",
+	  "amount": 22.55,
+	  "tipAmount": 3.38,
+	  "result": "SUCCESS",
+	},
+	{
+	  "id": "WT84KYFCQXMEWJE",
+	  "employee": {
+		"id": "4SCX9K33S7SKR",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 16:38:13",
+	  "amount": 29.98,
+	  "tipAmount": 4.5,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "1CHS0KGWZ485V64",
+	  "employee": {
+		"id": "4SCX9K33S7SKR",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 16:34:22",
+	  "amount": 72.19,
+	  "tipAmount": 3,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "N67XRG2KZ6T3B0J",
+	  "employee": {
+		"id": "4SCX9K33S7SKR",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:01:19",
+	  "amount": 100,
+	  "tipAmount": 0,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "7K7VWF1W60Z6HKG",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:13:58",
+	  "amount": 60.64,
+	  "tipAmount": 9.1,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "6SZA9WB5T8VRPF0",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:17:25",
+	  "amount": 30.31,
+	  "tipAmount": 8,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "8B5TH7X8854T0VT",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:25:53",
+	  "amount": 59.27,
+	  "tipAmount": 11.85,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "K534ZT3DYFREY58",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:23:52",
+	  "amount": 87.83,
+	  "tipAmount": 5,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "19XW542PAG12BSR",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:37:05",
+	  "amount": 51.97,
+	  "tipAmount": 0,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "0E1RH4P0V91AG04",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:30:33",
+	  "amount": 84.42,
+	  "tipAmount": 8,
+	  "result": "SUCCESS"
+	},
+	{
+	  "id": "01RH4P0V9FB1G04",
+	  "employee": {
+		"id": "279TP8BSACGPG",
+		"name": "Non"
+	  },
+	  "time": "2021-11-30 17:30:33",
+	  "amount": 999.99,
+	  "tipAmount": 8,
+	  "result": "SUCCESS"
+	}
+  ];
 
 //Writing to log for Clover
 function writeToLog(content){
@@ -91,18 +194,18 @@ async function start(testing){
 		}
 		else
 			await new  Promise(r => setTimeout(r, timeout * 5));
-		
     }
 
 }
 
 //the main function to be looped.
-//Will read a date/time in the database for the last poll time and poll Clover for new payments after that date/time
-//Will get open tickets from SambaPOS
+//Will read a date/time in the database for the last poll time and poll Clover for new payments after that date/time.
+//Will get open tickets from SambaPOS.
 //Iterate through the Clover Payments to check the charged amount against the remaining amount, and once found a match will settle that ticket.
 //If there are any remaining tickets it will check if two payments were made for one ticket or if two tickets were paid together.
-//Will set the database value for last read time to current time
+//Will set the database value for last read time to current time.
 //Insert payment data into database.
+//Will clear payment data other than the payments that failed.
 async function loop(){
 	let date = await samba.getCloverLastRead(delay + (timeout / 60000));
 	let paymentOptions = `filter=createdTime>=${date.getTime()}`;
@@ -116,68 +219,68 @@ async function loop(){
 	writeToLog("Tickets: " + JSON.stringify(tickets, undefined, 2));
 	let terminalId = await samba.openTerminal();
 	let unpaid = [];
-	for(let i in paymentData) { //Will check if open tickets have same amount as paid amount, and close said ticket.
+	for(let i in paymentData) { //Will check if open tickets have same amount as paid amount, and close said ticket. Payments that were not found will be added to an unpaid array.
 		if(paymentData[i].paid) continue;
 		let index = tickets.findIndex(tk => tk.remainingAmount == paymentData[i].amount);
 		if(index == -1){
-			unpaid.push(paymentData[i]);
+			unpaid.push({index:i, data:paymentData[i]});
 			continue;
 		}
-		await samba.payTicket(terminalId, tickets[index].id, paymentData[i].amount, paymentType);
+		let isPaid = await samba.payTicket(terminalId, tickets[index].id, paymentData[i].amount, paymentType);
 		tickets.splice(index, 1);
-		paymentData[i].paid = true;
+		paymentData[i].paid = isPaid;
 	}
-	if(unpaid.length > 0 && tickets.length > 0){
-		paidCount = 0;
-		for(let i in  unpaid){ //Will check if a ticket was paid with two different transaction and close appropriate ticket.
-			if(unpaid[i].paid) continue;
-			let j = parseInt(i) + 1;
-			while(j < unpaid.length){
-				let amount = round(unpaid[i].amount + unpaid[j].amount, 2);
+	if(unpaid.length > 0 && tickets.length > 0){//Will check if two payments were made for one ticket and close said ticket
+		for(let i = unpaid.length - 1; i >= 0; i--){
+			let j = i - 1;
+			while(j >= 0){
+				let amount = round(unpaid[i].data.amount + unpaid[j].data.amount, 2);
 				let index = tickets.findIndex(tk => amount == tk.remainingAmount);
 				if(index != -1){
-					paidCount += 2;
-					let indexes = [i,j];
-					for(let k in indexes){
-						let paymentIndex = paymentData.findIndex(payment => payment.id == unpaid[indexes[k]].id);
-						await samba.payTicket(terminalId, tickets[index].id, paymentData[paymentIndex].amount, paymentType);
-						unpaid[indexes[k]].paid = true;
-						paymentData[paymentIndex].paid = true;
-					}
-					tickets.splice(index, 1);
+					let isPaid = await samba.payTicket(terminalId, tickets[index].id, amount, paymentType);
+					paymentData[unpaid[i].index].paid = isPaid;
+					paymentData[unpaid[j].index].paid = isPaid;
+					unpaid.splice(i, 0);
+					unpaid.splice(j, 0);
+					tickets.splice(index, 0);
+					i -= 2;
 					break;
 				}
-				j++;
+				j--;
 			}
 		}
-		if(paidCount < unpaid.length){
-			for(let i = tickets.length - 1; i > 0; i--){
-				let j = i - 1;
-				while(j >= 0){//Will check if a two ticket was paid with one transaction and close appropriate tickets.
-					let amount = round(tickets[i].remainingAmount + tickets[j].remainingAmount, 2);
-					let index = unpaid.findIndex(payment => payment.amount == amount && !payment.paid);
-					if(index != -1){
-                        await samba.payTicket(terminalId, tickets[i].id, tickets[i].remainingAmount, paymentType);
-                        await samba.payTicket(terminalId, tickets[j].id, tickets[j].remainingAmount, paymentType);
-						unpaid[index].paid = true;
-						tickets.splice(i,1);
-						tickets.splice(j,1);
-						break;
-					}
-					j--;
+	}
+	if(unpaid.length > 0 && tickets.length > 0){//Will check if one payment paid for two tickets.
+		for(let i = tickets.length - 1; i >= 0; i--){
+			let j = i - 1;
+			while(j >= 0){
+				let amount = round(tickets[i].remainingAmount + tickets[j].remainingAmount, 2);
+				let index = unpaid.findIndex(payment => amount == payment.data.amount);
+				if(index != -1){
+					let iIsPaid = await samba.payTicket(terminalId, tickets[i].id, amount, paymentType);
+					let jIsPaid = await samba.payTicket(terminalId, tickets[j].id, amount, paymentType);
+					paymentData[unpaid[i].index].paid = iIsPaid;
+					paymentData[unpaid[j].index].paid = jIsPaid;
+					tickets.splice(i, 0);
+					tickets.splice(j, 0);
+					unpaid.splice(index, 0);
+					i -= 2;
+					break;
 				}
 			}
 		}
 	}
     await samba.closeTerminal(terminalId);
 	await samba.setCloverLastRead(readDate);
-    await sql.connect("Clover", paymentData);//inserting payment data into database.
+    await sql.connect("Clover", paymentData);//inserting payment data into database. TODO: renaming Clover to something more intuitive
+	paymentData = paymentData.filter(payment => payment.paid == false);
 }
 //Load employees registered on the Clover system.
 function loadEmployees(){
-	return getFromClover('employees',undefined)
+	return getFromClover('employees')
 		.then( data => {
 			employeeList = data;
+			console.log(data);
             return true;
     });
 }
@@ -224,9 +327,15 @@ function processData(payments){
 	let date = new Date();
 	readDate = new Date();
 	date.setMinutes(date.getMinutes() - 10);
-	payments = payments.filter(x => x.result == "SUCCESS" && new Date(x.createdTime) < date).map(x => {
-		x.employee = {id: x.employee.id,
-			name: `${employeeList.filter(y => y.id == x.employee.id)[0]?employeeList.filter(y => y.id == x.employee.id)[0].name:'No Name'}`};
+	payments = payments.filter(x => x.result == "SUCCESS" && new Date(x.createdTime) < date).map(async x => {
+		x.employee = {id: x.employee.id};
+		x.employee.name = employeeList.filter(y => y.id == x.employee.id)[0];
+		if(!x.employee.name){
+			await loadEmployees();
+			x.employee.name = employeeList.filter(y => y.id == x.employee.id)[0];
+			if(!x.employee.name)
+				x.employee.name = 'No Name';
+		}
 		return {
 			id: x.id,
 			employee: {
