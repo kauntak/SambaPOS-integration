e978f131038ad540b6193b0978c7124400bb53c0
kauntak <n.koshirae+github@gmail.com>
2021-12-11 00:55:29 -0700

    Update sql.js
    Major refactoring of code.
    Will use query, exec, insert function instead of different function calls bar queries used in multiple modules.
    
    Added pay ticket sql query.

--- sql.js
+++ sql.js
@@ -1,7 +1,8 @@
 //Connecting to database and executing queries.
-//TODO rename some functions to make them less confusing.(similair names for some.)
+//TODO: rename some functions to make them less confusing.(similair names for some.)
+//TODO: refactor code.
 
-module.exports = {connect, insertIntoPaymentsDB, insertIntoDeliverectDB, getHoldOrdersTotal};
+module.exports = {connect, exec, query, insert, insertIntoPaymentsDB, insertIntoDeliverectDB, payTicket};
 
 const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
@@ -42,6 +43,68 @@ function writeToLog(content){
 //write to log for SQL errors
 function writeToErrorLog(content){
 	log.write("SQL_Error", content);
+}
+
+
+function query(qry){
+	return new Promise((resolve, reject) => {
+		let connection = new Connection(config);
+		connection.on('connect', err => {
+			if(err)	reject(err);
+			else{
+				writeToLog('Connected To DB.');
+				Rqst = new Request(qry,(err,rowCount,rows) => {
+					if(err){
+						connection.close();
+						reject(err);
+					} else {
+						writeToLog(`Result: ${res}`);
+						connection.close();
+						resolve(res);
+					}
+				}).on('row', col =>{
+					let row = {};
+					for(let i in col){
+						row[col[i].metadata.colName] = col[i].value;
+					}
+					res.push(row);
+				});
+				connection.execSql(Rqst);
+			}
+		});
+		connection.connect();
+	});
+}
+
+function exec(qry){
+	console.log(qry);
+	return new Promise((resolve, reject) => {
+		let connection = new Connection(config);
+		connection.on('connect', err => {
+			if(err)	{console.log(err); reject(err);}
+			else{
+				writeToLog('Connected To DB.');
+				Rqst = new Request(qry,(err,rowCount,rows) => {
+					if(err){
+						console.log(err);
+						connection.close();
+						reject(err);
+					} else {
+						writeToLog(`Updated ${rowCount} rows.`);
+						connection.close();
+						resolve(true);
+					}
+				});
+				connection.execSql(Rqst);
+			}
+		});
+		connection.connect();
+	});
+}
+
+//TODO: create insert function
+function insert(qry){
+
 }
 
 //connecting to database.
@@ -180,6 +243,7 @@ function insertIntoDeliverectDB(data){
 	});
 }
 
+
 //Retrieve current hold orders from Database.
 //TODO: Change how Ticket data will be retrieved.
 //		Possibly accept a date variable?
@@ -189,13 +253,10 @@ function getHoldOrdersTotal(){
 	WHERE Name like 'pickup%'
 	order By Name`;
 	/*TODO: change qry to:
-	`DROP FUNCTION dbo.getPickupInterval
+	`DROP FUNCTION IF EXISTS dbo.getPickupInterval
 	GO
 
-	CREATE FUNCTION dbo.getPickupInterval
-	(
-	@input varchar(10)
-	)
+	CREATE FUNCTION dbo.getPickupInterval ( @input varchar(10) )
 	RETURNS varchar(10)
 	AS
 	BEGIN
@@ -218,16 +279,41 @@ function getHoldOrdersTotal(){
 	END
 	GO
 
-	SELECT SUM(TotalAmount), pickupTime
-	FROM (
-		SELECT TotalAmount, dbo.getPickupInterval(val) as pickupTime
-		FROM Tickets
-		CROSS APPLY OPENJSON(TicketTags) WITH (tagName varchar(30) '$.TN', val varchar(30) '$.TV') as pikcupTime
-		WHERE CAST(Date as date) = CAST(GETDATE() as date)
-		AND tagName = 'Pickup Time'
-	)a
-	GROUP BY pickupTime
-	ORDER BY pickupTime`
+	WITH CTE as (
+		SELECT SUM(TotalAmount) as TotalAmount, pickupTime, ROW_NUMBER() OVER( ORDER BY pickupTime) as rn
+		FROM (SELECT TotalAmount, dbo.getPickupInterval(val) as pickupTime
+			FROM Tickets
+			CROSS APPLY OPENJSON(TicketTags) WITH (tagName varchar(30) '$.TN', val varchar(30) '$.TV') as pickupTime
+			WHERE CAST(Date as date) = CAST(GETDATE() - 3 as date)
+			AND tagName = 'Pickup Time'
+			UNION
+			SELECT TotalAmount, pickupTime
+			FROM (Values
+				(0,'16:30'), 
+				(0,'16:45'), 
+				(0,'17:00'), 
+				(0,'17:15'), 
+				(0,'17:30'), 
+				(0,'17:45'), 
+				(0,'18:00'), 
+				(0,'18:15'), 
+				(0,'18:30'), 
+				(0,'19:00'), 
+				(0,'19:15'), 
+				(0,'19:30'), 
+				(0,'19:45'), 
+				(0,'20:00'), 
+				(0,'20:15'), 
+				(0,'20:30')
+			) AS emptyTimeTable(TotalAmount, pickupTime)
+		)a
+		GROUP BY pickupTime
+	)
+
+
+	SELECT TotalAmount, pickupTime
+	FROM CTE
+	WHERE rn >= (SELECT rn FROM CTE WHERE pickupTime = '16:30')`
 	*/
 	return new Promise((resolve, reject) => {
 		let res = [];
@@ -325,4 +411,283 @@ function getCurrentTotals(){
 	}).catch(err => {
 		writeToErrorLog("QUERY:" + qry + "\r\nERROR:" + err);
 	});
+}
+
+
+//Pay ticket via SQL
+function payTicket(ticketId, amount, paymentType){
+	let qry = `
+	DECLARE @PAID_AMOUNT decimal(16, 2) = ${amount};
+DECLARE @TICKET_ID int = ${ticketId};
+DECLARE @PAYMENT_TYPE varchar(30) = '${paymentType}';
+
+DECLARE @TICKET_NUMBER int;
+DECLARE @REMAINING_AMOUNT decimal(16,2);
+DECLARE @TICKET_TAX_AMOUNT decimal(16,2);
+DECLARE @TICKET_DEPARTMENT_ID int;
+DECLARE @IS_FULLY_PAID bit;
+DECLARE @AMOUNT_BEFORE_TAX decimal(16,2);
+
+DECLARE @PAYMENT_TYPE_ID int;
+DECLARE @DOCUMENT_ID int;
+DECLARE @TRANSACTION_ID int;
+DECLARE @TRANSACTION_TYPE varchar(20);
+DECLARE @TRANSACTION_NAME varchar(50);
+
+
+SELECT @TICKET_NUMBER = TicketNumber, 
+    @REMAINING_AMOUNT = (RemainingAmount - @PAID_AMOUNT),
+    @IS_FULLY_PAID = CASE 
+        WHEN RemainingAmount - @PAID_AMOUNT = 0 THEN 1 
+        ELSE 0
+        END,
+    @TICKET_DEPARTMENT_ID = DepartmentId,
+    @TICKET_TAX_AMOUNT = CASE
+        WHEN TotalAmountPreTax = TotalAmount THEN 0
+        ELSE TotalAmount - TotalAmountPreTax
+        END,
+    @AMOUNT_BEFORE_TAX = TotalAmountPreTax
+FROM [SambaPOS5].[dbo].[Tickets]
+WHERE Id = @TICKET_ID;
+
+SELECT @PAYMENT_TYPE_ID = Id
+FROM [SambaPOS5].[dbo].[PaymentTypes]
+WHERE Name = @PAYMENT_TYPE;
+
+
+UPDATE [dbo].[Tickets]  SET 
+	  [LastUpdateTime] = CURRENT_TIMESTAMP
+	, [TicketVersion] = CURRENT_TIMESTAMP
+	, [LastPaymentDate] = CURRENT_TIMESTAMP
+	, [IsClosed] = @IS_FULLY_PAID
+	, [RemainingAmount] = @REMAINING_AMOUNT
+	, [TicketStates] = CASE
+        WHEN @IS_FULLY_PAID = 0  THEN [TicketStates]
+        ELSE REPLACE([TicketStates], '"Unpaid"', '"Paid"')
+        END
+WHERE Id = @TICKET_ID;
+
+INSERT INTO [SambaPOS5].[dbo].[AccountTransactionDocuments]
+(Date, DocumentTypeId, Name, UserId, UserName)
+Values(
+    CURRENT_TIMESTAMP,
+    (SELECT Id FROM [SambaPOS5].[dbo].[AccountTransactionDocumentTypes] WHERE DescriptionTemplate like CONCAT(@PAYMENT_TYPE, '%')),
+    CONCAT('Ticket Transaction [#',CAST(@TICKET_NUMBER as varchar), ']'),
+    0,
+    '*'
+);
+SELECT @DOCUMENT_ID = SCOPE_IDENTITY();
+
+DECLARE @PAYMENT_BEFORE_TAX decimal(16, 2) = CASE
+    WHEN @IS_FULLY_PAID = 1 THEN @AMOUNT_BEFORE_TAX
+    ELSE @PAID_AMOUNT / 1.05
+	END;
+INSERT INTO [SambaPOS5].[dbo].[AccountTransactions]
+(AccountTransactionDocumentId, Amount, ExchangeRate, AccountTransactionTypeId, SourceAccountTypeId, TargetAccountTypeId, IsReversed, Reversable, Name)
+Values
+(
+    @DOCUMENT_ID,
+    @PAYMENT_BEFORE_TAX,
+    1.0000000000, 
+    (SELECT Id FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Sale Transaction'),
+    (SELECT SourceAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Sale Transaction'),
+    (SELECT TargetAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Sale Transaction'),
+    0,
+    1,
+    CONCAT('Sale Transaction [#', @TICKET_NUMBER, ']')
+);
+SELECT @TRANSACTION_ID = Id, @TRANSACTION_TYPE = AccountTransactionTypeId, @TRANSACTION_NAME = Name FROM [SambaPOS5].[dbo].[AccountTransactions] WHERE Id = SCOPE_IDENTITY();
+
+INSERT INTO [SambaPOS5].[dbo].[AccountTransactionValues](
+      [AccountTransactionId]
+	, [AccountTransactionDocumentId]
+	, [DepartmentId]
+	, [AccountTypeId]
+	, [AccountId]
+	, [Date]
+	, [Debit]
+	, [Credit]
+	, [Exchange]
+	, [AccountTransactionTypeId]
+	, [Name])
+Values(
+     @TRANSACTION_ID
+    ,@DOCUMENT_ID
+    ,@TICKET_DEPARTMENT_ID
+    ,2
+    ,1
+    ,CURRENT_TIMESTAMP
+    ,0
+    ,@PAYMENT_BEFORE_TAX
+    ,-@PAYMENT_BEFORE_TAX
+    ,@TRANSACTION_TYPE
+    ,@TRANSACTION_NAME
+),
+(
+     @TRANSACTION_ID
+    ,@DOCUMENT_ID
+    ,@TICKET_DEPARTMENT_ID
+    ,1
+    ,2
+    ,CURRENT_TIMESTAMP
+    ,@PAYMENT_BEFORE_TAX
+    ,0
+    ,@PAYMENT_BEFORE_TAX
+    ,@TRANSACTION_TYPE
+    ,@TRANSACTION_NAME
+)
+
+IF EXISTS(SELECT * FROM (SELECT @TICKET_TAX_AMOUNT as Amount)a WHERE Amount != 0)
+    BEGIN
+        DECLARE @TAX_AMOUNT decimal(16,2) = CASE
+            WHEN @REMAINING_AMOUNT = 0 THEN  @TICKET_TAX_AMOUNT
+            ELSE @PAID_AMOUNT - @PAYMENT_BEFORE_TAX
+            END
+        INSERT INTO [SambaPOS5].[dbo].[AccountTransactions]
+        (AccountTransactionDocumentId, Amount, ExchangeRate, AccountTransactionTypeId, SourceAccountTypeId, TargetAccountTypeId, IsReversed, Reversable, Name)
+        Values
+        (
+            @DOCUMENT_ID,
+            @TAX_AMOUNT,
+            1.0000000000, 
+            (SELECT Id FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = '5% GST Processes'),
+            (SELECT SourceAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = '5% GST Processes'),
+            (SELECT TargetAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = '5% GST Processes'),
+            0,
+            1,
+            '5% GST Processes'
+        );
+        SELECT @TRANSACTION_ID = Id, @TRANSACTION_TYPE = AccountTransactionTypeId, @TRANSACTION_NAME = Name FROM [SambaPOS5].[dbo].[AccountTransactions] WHERE Id = SCOPE_IDENTITY();
+
+        INSERT INTO [SambaPOS5].[dbo].[AccountTransactionValues](
+            [AccountTransactionId]
+            , [AccountTransactionDocumentId]
+            , [DepartmentId]
+            , [AccountTypeId]
+            , [AccountId]
+            , [Date]
+            , [Debit]
+            , [Credit]
+            , [Exchange]
+            , [AccountTransactionTypeId]
+            , [Name])
+        Values(
+            @TRANSACTION_ID
+            ,@DOCUMENT_ID
+            ,@TICKET_DEPARTMENT_ID
+            ,2
+            ,8
+            ,CURRENT_TIMESTAMP
+            ,0
+            ,@TAX_AMOUNT
+            ,-@TAX_AMOUNT
+            ,@TRANSACTION_TYPE
+            ,@TRANSACTION_NAME
+        ),
+        (
+            @TRANSACTION_ID
+            ,@DOCUMENT_ID
+            ,@TICKET_DEPARTMENT_ID
+            ,1
+            ,2
+            ,CURRENT_TIMESTAMP
+            ,@TAX_AMOUNT
+            ,0
+            ,@TAX_AMOUNT
+            ,@TRANSACTION_TYPE
+            ,@TRANSACTION_NAME
+        )
+    END
+
+INSERT INTO [SambaPOS5].[dbo].[AccountTransactions]
+(AccountTransactionDocumentId, Amount, ExchangeRate, AccountTransactionTypeId, SourceAccountTypeId, TargetAccountTypeId, IsReversed, Reversable, Name)
+Values
+(
+    @DOCUMENT_ID,
+    @PAID_AMOUNT,
+    1.0000000000, 
+    (SELECT Id FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Payment Transaction'),
+    (SELECT SourceAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Payment Transaction'),
+    (SELECT TargetAccountTypeId FROM [SambaPOS5].[dbo].[AccountTransactionTypes] WHERE Name = 'Payment Transaction'),
+    0,
+    1,
+    CONCAT('Payment Transaction [', @PAYMENT_TYPE, ']')
+);
+SELECT @TRANSACTION_ID = Id, @TRANSACTION_TYPE = AccountTransactionTypeId, @TRANSACTION_NAME = Name FROM [SambaPOS5].[dbo].[AccountTransactions] WHERE Id = SCOPE_IDENTITY();
+
+INSERT INTO [SambaPOS5].[dbo].[AccountTransactionValues](
+      [AccountTransactionId]
+	, [AccountTransactionDocumentId]
+	, [DepartmentId]
+	, [AccountTypeId]
+	, [AccountId]
+	, [Date]
+	, [Debit]
+	, [Credit]
+	, [Exchange]
+	, [AccountTransactionTypeId]
+	, [Name])
+Values(
+     @TRANSACTION_ID
+    ,@DOCUMENT_ID
+    ,@TICKET_DEPARTMENT_ID
+    ,1
+    ,2
+    ,CURRENT_TIMESTAMP
+    ,0
+    ,@PAID_AMOUNT
+    ,-@PAID_AMOUNT
+    ,@TRANSACTION_TYPE
+    ,@TRANSACTION_NAME
+),
+(
+     @TRANSACTION_ID
+    ,@DOCUMENT_ID
+    ,@TICKET_DEPARTMENT_ID
+    ,3
+    ,6
+    ,CURRENT_TIMESTAMP
+    ,@PAID_AMOUNT
+    ,0
+    ,@PAID_AMOUNT
+    ,@TRANSACTION_TYPE
+    ,@TRANSACTION_NAME
+)
+
+--paymentTypeId: 2 for Credit Card, 7 for Online Payment
+INSERT INTO [SambaPOS5].[dbo].[Payments](
+	  [TicketId]
+	, [PaymentTypeId]
+	, [DepartmentId]
+	, [Name]
+	, [Date]
+	, [AccountTransactionId]
+	, [Amount]
+	, [TenderedAmount]
+	, [UserId]
+	, [TerminalId]
+	, [ExchangeRate]
+	, [PaymentData]
+	, [CanAdjustTip]
+	, [AccountTransaction_Id]
+	, [AccountTransaction_AccountTransactionDocumentId]
+	) 
+VALUES (
+       @TICKET_ID
+     , @PAYMENT_TYPE_ID
+     , 1
+     , @PAYMENT_TYPE
+     , CURRENT_TIMESTAMP
+     , @TRANSACTION_ID
+     , @PAID_AMOUNT
+     , @PAID_AMOUNT
+     , 0
+     , 0
+     , 1.0000000000
+     , ''
+     , 0
+     , @TRANSACTION_ID
+     , @DOCUMENT_ID
+)`;
+	return exec(qry);
 }
